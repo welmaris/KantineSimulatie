236adc9a0bc0f366fc5eecc9d83f7ddb79ca3b66
Esther <estherzr97@gmail.com>
2020-05-14 11:38:41 +0200

    set-up

---
+++ src/main/java/Datum.java
@@ -0,0 +1,28 @@
+package main.java;
+
+public class Datum {
+
+	private int dag;
+	private int maand;
+	private int jaar;
+
+	/**
+	 * Constructor
+	 */
+	// TODO
+
+	public boolean bestaatDatum(int dag, int maand, int jaar) {
+		// TODO
+		return false;
+	}
+
+	/**
+	 * Getter voor Sting weergave van datum
+	 *
+	 * @return Geboortedatum
+	 */
+	public String getDatumAsString() {
+		// TODO
+		return "";
+	}
+}
---
+++ src/main/java/KortingskaartHouder.java
@@ -0,0 +1,19 @@
+package main.java;
+
+public interface KortingskaartHouder {
+
+    /**
+     * Methode om kortingspercentage op te vragen
+     */
+    public double geefKortingsPercentage();
+
+    /**
+     * Methode om op te vragen of er maximum per keer aan de korting zit
+     */
+    public boolean heeftMaximum();
+
+    /**
+     * Methode om het maximum kortingsbedrag op te vragen
+     */
+    public double geefMaximum();
+}
