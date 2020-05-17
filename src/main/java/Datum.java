47dbbbffd29b4a460c6056d239e1bcd3bc91487b
Jojootjes <65397340+Jojootjes@users.noreply.github.com>
2020-05-17 12:58:40 +0200

    methode bestaatDatum
    methode bstaat datum aangemaakt
    constructors gemaakt en commentaar toegevoegd

--- src/main/java/Datum.java
+++ src/main/java/Datum.java
@@ -1,3 +1,5 @@
+package main.java;
+
 public class Datum {
 
 	private int dag;
@@ -6,35 +8,71 @@ public class Datum {
 
 	/**
 	 * Constructor
+	 * @param dag
+	 * @param maand
+	 * @param jaar
+	 *
 	 */
 	public Datum(int dag, int maand, int jaar){
-		this.dag = dag;
-		this.maand = maand;
-		this.jaar = jaar;
+		if(bestaatDatum(dag, maand, jaar)) {
+			;
+			this.dag = dag;
+			this.maand = maand;
+			this.jaar = jaar;
+		} else {this.dag = 0;
+			this.maand = 0;
+			this.jaar = 0;};
 	}
-	// TODO
+
+	/**
+	 * Constructor zonder parameter
+	 */
+	public Datum() {
+		dag = 0;
+		maand = 0;
+		jaar = 0;
+
+	}
 
 	public boolean bestaatDatum(int dag, int maand, int jaar) {
 		// TODO
 
-		//als de datum nul is geeft hij false
-		//als de maand niet tussen de 1 en de 12 is
-		//als het jaar niet tussen de 1900 en 2100 ligt
-		//de dag maand combinatie moet bestaan
-		//schrikkeljaar als deelbaar door 4, maar niet 100 -->
 
-		//hier moet 29 & 30 februari nog in worden verwerjt
-		if (jaar % 4 = 0){
+
+		//hier word gecontroleerd of de maand tussen de 1 en de 12 is
+		//hier word gecontroleerd of de dag tussen de 1 en de 31 ligt
+		//hier word gecontroleerd of het jaar tussen de  1900 en de 2100 ligt
+		if ( maand < 1  ||maand > 12 || dag < 1 || dag > 31 || jaar < 1900 || jaar > 2100) {
 			return false;
 		}
-
-		if(maand = 0){
-
-			return false;
+		//hier word gecontroleerd of de maand februari is
+		if (maand == 2 ){
+			//hier word gecontroleerd of het een schrikkeljaar is
+			if (jaar % 4 == 0) {
+				//hier word gecontroleerd of het een uitzondering is op het schrikkeljaar
+				if (jaar % 100 == 0) {
+					//hier word gecontroleerd of het geen uitzondering op de uitzondering is en dus WEL een schrikkeljaar is
+					if (jaar % 400 == 0 && dag < 30) {
+						return true;
+					} else if (dag < 29) {
+						return true;
+					}
+				} else if (dag < 30) {
+					return true;
+				}
+			} else if (dag < 29){return true;}
 		}
-
-
+		if (maand == 1 || maand == 3 || maand == 5|| maand == 7|| maand == 8|| maand== 10|| maand == 12){
+			if (dag < 32){return true;}
+			else return false;
+		}
+		if ( maand == 4|| maand == 6|| maand == 9 || maand == 11){
+			if (dag<31){return true;}
+			else return false;
+		}
+		return false;
 	}
+
 
 
 
@@ -45,7 +83,7 @@ public class Datum {
 	 */
 	public String getDatumAsString() {
 		//het aanmaken van een string variabele
-		public String datum = dag + "/" + maand + "/" + jaar;
+		 String datum = dag + "/" + maand + "/" + jaar;
 
 
 		return datum;
