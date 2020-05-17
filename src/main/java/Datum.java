bb052103d9e632a0eede8c4afbd6d1944174a7e0
Esther <estherzr97@gmail.com>
2020-05-17 09:37:34 +0200

    Update Datum.java
    werk van Julia, heb wel een constructor toegevoegd zodat andere klassen een datum kunnen hebben
    
    Co-Authored-By: jojootjes <jojootjes@users.noreply.github.com>

--- src/main/java/Datum.java
+++ src/main/java/Datum.java
@@ -1,5 +1,3 @@
-package main.java;
-
 public class Datum {
 
 	private int dag;
@@ -9,12 +7,36 @@ public class Datum {
 	/**
 	 * Constructor
 	 */
+	public Datum(int dag, int maand, int jaar){
+		this.dag = dag;
+		this.maand = maand;
+		this.jaar = jaar;
+	}
 	// TODO
 
 	public boolean bestaatDatum(int dag, int maand, int jaar) {
 		// TODO
-		return false;
+
+		//als de datum nul is geeft hij false
+		//als de maand niet tussen de 1 en de 12 is
+		//als het jaar niet tussen de 1900 en 2100 ligt
+		//de dag maand combinatie moet bestaan
+		//schrikkeljaar als deelbaar door 4, maar niet 100 -->
+
+		//hier moet 29 & 30 februari nog in worden verwerjt
+		if (jaar % 4 = 0){
+			return false;
+		}
+
+		if(maand = 0){
+
+			return false;
+		}
+
+
 	}
+
+
 
 	/**
 	 * Getter voor Sting weergave van datum
@@ -22,7 +44,21 @@ public class Datum {
 	 * @return Geboortedatum
 	 */
 	public String getDatumAsString() {
-		// TODO
-		return "";
+		//het aanmaken van een string variabele
+		public String datum = dag + "/" + maand + "/" + jaar;
+
+
+		return datum;
+	}
+	public int getDag() {
+		return dag;
+	}
+
+	public int getMaand() {
+		return maand;
+	}
+
+	public int getJaar() {
+		return jaar;
 	}
 }
