d3a4a2d0ddaa94dcc5e3d2a64b8536b7ea1f017d
Esther <estherzr97@gmail.com>
2020-05-20 12:02:55 +0200

    Update KantineSimulatie_1.java

--- src/main/java/KantineSimulatie_1.java
+++ src/main/java/KantineSimulatie_1.java
@@ -22,7 +22,7 @@ public class KantineSimulatie {
     public void simuleer(int dagen) {
 
         // herhaal voor elke dag
-        for (int i = 0; ...) {
+        for (int i = 0; i < dagen; i++) {
 
             // per dag nu even vast 10 + i personen naar binnen
             // laten gaan, wordt volgende week veranderd...
@@ -30,13 +30,17 @@ public class KantineSimulatie {
             // for lus voor personen
             for (int j = 0; j < 10 + i; j++) {
                 // kantine.(...);
+                kantine.loopPakSluitAan();
             }
 
             // verwerk rij voor de kassa
+            kantine.verwerkRijVoorKassa();
 
             // toon dagtotalen (artikelen en geld in kassa)
+            kantine.hoeveelheidGeldInKassa();
 
             // reset de kassa voor de volgende dag
+            kantine.resetKassa();
         }
     }
 
@@ -52,6 +56,7 @@ public class KantineSimulatie {
             dagen = Integer.parseInt(args[0]);
         }
 
-        simulate(dagen);
+        //simulate(dagen);
+        simuleer(dagen);
     }
 }
